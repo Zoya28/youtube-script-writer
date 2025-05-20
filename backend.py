bc826398c2b655aa80b9277df5ab80c55d69d5f8
Zoya Qureshi <111566341+Zoya28@users.noreply.github.com>
2025-05-20 13:31:04 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,57 +2,46 @@ from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain_community.tools import DuckDuckGoSearchRun
-
 import time
 
 
-
 # function to generate script
-def generate(api_key,topic, video_length, creativity):
-    time.sleep(2)  # Add delay between requests
+def generate(api_key, topic, video_length, creativity):
+    time.sleep(2)
 
-    # template for generating title
     title_template = PromptTemplate(
         input_variables=["topic"],
         template="Generate a creative title video on topic {topic}.",
     )
 
-    # template for generating script
     script_template = PromptTemplate(
         input_variables=["topic", "video_length", "DuckDuckGo_search"],
         template="Generate a script for a {video_length}-minute video on topic {topic} using this data {DuckDuckGo_search}.",
     )
-
-    # set up the llm
-    llm = ChatGroq(
-        model="llama-3.1-8b-instant",
-        temperature=creativity,
-        max_tokens=1000,
-        api_key= api_key
-    )
-
-    # Creating chains for title and script generation
-    title_chain = LLMChain(llm=llm, prompt = title_template, verbose=True)
-    script_chain = LLMChain(llm=llm, prompt = script_template, verbose=True)
-
-    # Creating DuckDuckGo search tool
-    search = DuckDuckGoSearchRun()
-
-    title = title_chain.invoke({"topic": topic})
-    title_result = title["text"]
-
-    # DuckDuckGo search
     try:
-        search_result = search.run(topic)
-    except Exception as e:
-        search_result = "Search failed due to rate limit or other issue."
-        print(f"Search error: {e}")
+        llm = ChatGroq(
+            model="llama-3.1-8b-instant",
+            temperature=creativity,
+            max_tokens=1000,
+            api_key=api_key,
+        )
 
-    script = script_chain.run(
-        {
-            "topic": topic,
-            "video_length": video_length,
-            "DuckDuckGo_search": search_result,
-        }
-    )
-    return search_result, title, script
+        title_chain = LLMChain(llm=llm, prompt = title_template, verbose=True)
+        script_chain = LLMChain(llm=llm, prompt = script_template, verbose=True)
+
+        # Creating DuckDuckGo search tool
+        search = DuckDuckGoSearchRun()
+        title = title_chain.invoke({"topic": topic})
+        search_result = search.run(topic)    
+
+        script = script_chain.run(
+            {
+                "topic": topic,
+                "video_length": video_length,
+                "DuckDuckGo_search": search_result,
+            }
+        )
+        return search_result, title, script
+
+    except Exception as e:
+        return f"Enter correct Groq API key! Error: {e}",None,None
